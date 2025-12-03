d78d5c8a33c13bb5f2ef98d254805127c0a83051
RickPrimeC137 <bendernounet@gmail.com>
2025-12-03 14:40:06 +0100

    Update constants.js

--- public/js/config/constants.js
+++ public/js/config/constants.js
@@ -29,7 +29,7 @@ export const GAME_H = 1536;
  * < 1 = dézoomer (tout plus petit)
  * > 1 = zoomer (tout plus gros)
  */
-export const GAME_ZOOM = 0.5; // ajuste entre 0.8 et 1.0 selon ton feeling
+export const GAME_ZOOM = 0.8; // ajuste entre 0.8 et 1.0 selon ton feeling
 
 // ============================================================
 // PROFIL PHYSIQUE DU JEU
@@ -274,5 +274,6 @@ export const STORAGE_KEYS = {
   LANG: LANG_STORAGE_KEY,
   HARD_MODE: HARD_MODE_KEY
 };
+
 
 
